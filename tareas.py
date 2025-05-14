ca2ebcaff9e7bd9961779c839485aae53e8b36e0
davidprados99 <davidprados99@gmail.com>
2025-05-14 09:44:50 +0200

    Tercer commit
    Añado el método eliminar_tarea

--- tareas.py
+++ tareas.py
@@ -1,3 +1,8 @@
 def agregar_tarea(lista, tarea):    
     lista.append(tarea)    
     return lista
+
+def eliminar_tarea(lista,indice):
+    for i, tarea in enumerate(lista):
+        if i==indice:
+            lista.remove(tarea)
